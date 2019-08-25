752e0929c15b84cb5a1bc05dd6924bcbdb4352a9
Abner Ye <741317822@qq.com>
2019-08-25 13:39:19 -0400

    main function

--- main.cu
+++ main.cu
@@ -5,10 +5,17 @@ using namespace std;
 
 int main()
 {
-    const int N = 128;
-    const int ITER = 10000;
-    test_temperature(N, ITER);
-    // show_lattice(N, 0.4407, ITER);
+    const int N = 1024;
+    const int ITER = 40000;
+    // test_temperature(N, ITER);
+    show_lattice(N, 0.4407, ITER);
+    // ---- the codes below is for test_observable at a given temperature ---- //
+    /*lattice sigma(N);
+    sigma.initialize('r');
+    ofstream fout;
+    fout.open("data.txt");
+    test_observable(sigma, 0.50, ITER, fout);
+    fout.close();*/
 }
 
 // nvc main.cu test.cu lattice.cu Metropolis.cu
