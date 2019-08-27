0c6ad8c556379b2cb970fb6d4687e525f426c610
Abner Ye <741317822@qq.com>
2019-08-26 22:28:43 -0400

    Update main.cu

--- main.cu
+++ main.cu
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    const int N = 128;
+    const int N = 512;
     const int ITER = 10000;
     // test_temperature(N, ITER);
     // show_lattice(N, 0.4407, ITER);
@@ -14,7 +14,7 @@ int main()
     sigma.initialize('1');
     ofstream fout;
     fout.open("data.txt");
-    test_observable(sigma, 0.4407, ITER, fout);
+    test_observable(sigma, 0.45, ITER, fout);
     fout.close();
 }
 
